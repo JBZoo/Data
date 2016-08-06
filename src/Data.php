9f9fbb4b5b10042fd540d2dd720eeae53f9240ef
smetdenis <admin@jbzoo.com>
2016-08-06 18:56:29 +0700

    add flag ARRAY_AS_PROPS

--- src/Data.php
+++ src/Data.php
@@ -31,7 +31,6 @@ class Data extends \ArrayObject
      */
     public function __construct($data = array())
     {
-        $this->setFlags(\ArrayObject::STD_PROP_LIST);
         $this->setFlags(\ArrayObject::ARRAY_AS_PROPS);
 
         if ($data && is_string($data) && file_exists($data)) {
