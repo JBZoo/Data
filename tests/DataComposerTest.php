e8c74cf3d7bd2c061538512cd1ad74932ad04dfe
Denis Smetannikov <smet.denis@gmail.com>
2020-07-15 02:48:59 +0300

    Fixes versions

---
+++ tests/DataComposerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Data
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Data
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/Data
+ * @author     Denis Smetannikov <denis@jbzoo.com>
+ */
+
+namespace JBZoo\PHPUnit;
+
+/**
+ * Class DataComposerTest
+ *
+ * @package JBZoo\PHPUnit
+ */
+class DataComposerTest extends AbstractComposerTest
+{
+
+}
