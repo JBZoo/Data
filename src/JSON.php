7b73950434d54c5bd5b99586f8ffb3920c55b87f
Denis Smet <denis.smetannikov@yandex.ru>
2023-03-09 13:21:38 +0400

    Prefer stable versions

--- src/JSON.php
+++ src/JSON.php
@@ -26,6 +26,9 @@ final class JSON extends Data
 
     protected function encode(array $data): string
     {
-        return \json_encode($data, \JSON_THROW_ON_ERROR | \JSON_PRETTY_PRINT | \JSON_BIGINT_AS_STRING);
+        $result = \json_encode($data, \JSON_THROW_ON_ERROR | \JSON_PRETTY_PRINT | \JSON_BIGINT_AS_STRING);
+
+        // @phpstan-ignore-next-line
+        return $result ?: '';
     }
 }
